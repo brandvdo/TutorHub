eb64f889dce13f8bcd207b3aac959c9d2a184519
swisscheesey <thiccpfsognr@gmail.com>
2022-10-04 15:09:26 -0500

    literally nothing

---
+++ Client/screens/SignUpScreen.js
@@ -0,0 +1,23 @@
+/*
+
+    User Profile Screen
+
+*/
+
+import React from 'react';
+import {StyleSheet, View, Text} from 'react-native';
+
+const UserProfileScreen = () =>{
+    return(
+        <View>
+            <Text>User Profile Screen</Text>
+        </View>
+    );
+
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default UserProfileScreen;
