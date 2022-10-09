76932751942e6cd500258413b02617f3aa78274e
swisscheesey <thiccpfsognr@gmail.com>
2022-10-08 22:35:15 -0500

    Update SignUpScreen.js

--- Client/screens/SignUpScreen.js
+++ Client/screens/SignUpScreen.js
@@ -1,16 +1,27 @@
 /*
 
-    User Profile Screen
+    Sign Up Screen
 
 */
 
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, TextInput} from 'react-native';
 
 const UserProfileScreen = () =>{
+    const [text, setText] = useState('');
     return(
         <View>
-            <Text>User Profile Screen</Text>
+            <Text>Sign Up Screen</Text>
+            <TextInput
+            style={{height: 40}}
+            placeholder="PlaceholderText"
+            onChangeText={newText => setText(newText)}
+            defaultValue={text}
+            />
+            <Text style={{padding: 10, fontSize:42}}>
+                
+
+            </Text>
         </View>
     );
 
