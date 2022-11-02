5480015176d39083912257f1bd8cfa4fd3754e3c
tbeaver23 <tbeaver23@yahoo.com>
2022-11-02 01:24:58 -0500

    storing tokens

--- Client/screens/mainFeed/MainFeed.js
+++ Client/screens/mainFeed/MainFeed.js
@@ -9,13 +9,14 @@
 /*
     TODO: save token and store on user ID, get to work with creating accounts
 */
-const MainFeed = ( )=> {
 
+const MainFeed = () => {
     fetch("http://70.177.34.147:3000/api/users/login", {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
-            'Content-Type': 'application/json'
+            'Content-Type': 'application/json',
+            'auth-token': 'jwtToken'
         },
         body: JSON.stringify({
             email: "brandvdo@gmail.com",
@@ -29,6 +30,8 @@ const MainFeed = ( )=> {
                 "POST Response",
                 "Response Body -> " + JSON.stringify(responseData)
             )
+            //const user_Token = sessionStorage.setItem('jwtToken',JSON.stringify(responseData.token));
+           // console.log(user_Token);
         })
         .done();
 }
