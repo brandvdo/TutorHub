ea8d4dc15a0603cc3cb381f5ab49540bbd1cfe49
Troy Zelden <92113845+CaptPenguin@users.noreply.github.com>
2022-11-03 14:05:21 -0500

    Profile Screen Idea
    I am still experimenting but I put in sometime on how the top screen would look like

--- Client/screens/UserProfileScreen.js
+++ Client/screens/UserProfileScreen.js
@@ -5,23 +5,69 @@
 */
 
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
+import {StyleSheet, View, Text, TouchableOpacity, Image} from 'react-native';
 
 const styles = StyleSheet.create({
-    Header: {
-        paddingTop: 50,
+    container:{
+        flex:1,
         backgroundColor: '#05998c',
+        padding: 10,
+    },
+    image:{
+        alignSelf: 'flex-start',
+        width: 120,
+        height: 120,
+        marignLeft:180,
+        marginTop: 40,
+    },
+    profilePic:{
+        alignSelf: 'flex-end',
+        marginLeft: 60,
+        width: 120,
+        height: 120,
+
+    },
+    row:{
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+    },
+    button:{
+        paddingHorizontal: 20,
+        paddingVertical: 8,
+        borderRadius: 30,
+        backgroundColor: "#e0e0e0",
+        alignSelf: "flex-end",
+        marginRight: 45,
+        marginTop: 10,
+        textAlign: "center",
+    },
+    buttonLabel:{
+        fontSize: 12,
+        color: "black",
     }
 });
 
 const UserProfileScreen = () =>{
     return(
-        <View>
-            <View style={styles.Header}/>
-            <View>
-                <Text>
-                    test
+        <View style={[styles.container]}>
+            <View style={styles.row}>
+            <Image 
+                source={require('../assets/TutorHub.png')}
+                style={[styles.image]}>
+            </Image>
+            <Image 
+                source={require('../assets/images/user-profile-icon-free-vector.webp')}
+                style={[styles.profilePic]}>
+            </Image>
+            </View>
+            <TouchableOpacity 
+                style={[styles.button]}>
+
+                <Text style={[styles.buttonLabel]}> Choose your profile pic                   
                 </Text>
+            </TouchableOpacity>
+            <View style={styles.row}>
+                
             </View>
         </View>
     );
