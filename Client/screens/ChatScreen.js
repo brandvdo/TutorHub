fa8527f545f284ad75d3186e96a59baaaf8b608b
tbeaver23 <tbeaver23@yahoo.com>
2022-11-14 17:34:54 -0600

    friends list button

--- Client/screens/ChatScreen.js
+++ Client/screens/ChatScreen.js
@@ -4,19 +4,34 @@ This is for testing only remove later
 
 */
 import React, { useState, useEffect } from 'react';
-import {StyleSheet, View, Text, FlatList} from 'react-native';
+import {StyleSheet, View, Text, FlatList, TouchableOpacity} from 'react-native';
 
 const styles = StyleSheet.create({
     Header: {
         paddingTop: 50,
         backgroundColor: '#05998c',
-    }
+    },
+    buttonBio:{
+        paddingHorizontal: 10,
+        paddingVertical: 5,
+        borderRadius: 30,
+        backgroundColor: "#e0e0e0",
+        marginTop: 10,
+        marginBottom: 10,
+        alignSelf: 'center',
+    },
 });
 
 const ChatScreen = () =>{
     return(
         <View>
-            <View style={styles.Header}/>
+            <View style={styles.Header}>
+            <View style={styles.buttonBio}>
+                <TouchableOpacity onPress={() => navigation.navigate('UserProfile')}>
+                    <Text>Friend's List</Text>
+                </TouchableOpacity>
+            </View>
+            </View>
             <View>
 
             </View>
