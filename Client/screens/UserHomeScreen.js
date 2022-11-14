0d22e5aadf07490144c18ec19b15e30a9308da8c
Brandon Do <brandvdo@gmail.com>
2022-11-13 18:24:40 -0600

    Home page now gets user's feed

--- Client/screens/UserHomeScreen.js
+++ Client/screens/UserHomeScreen.js
@@ -33,12 +33,15 @@ const UserHomeScreen = () => {
 
     const [data, setData] = useState([]);
     const [loading, setLoading] = useState(true);
+/*
 
-    //TODO fix api request
-    const fetchData = async () => {
+    Getting invalid json error?
+
+*/
+    const fetchUserInfo = async () => {
         const userToken = await SecureStore.getItemAsync("token");
-        console.log(userToken)
-        const resp = await fetch("http://70.177.34.147:3000/api/home/newsFeed/messages", {
+        const decodedToken = jwtDecode(userToken);
+        const resp = await fetch("http://70.177.34.147:3000/api/home/newsFeed/messages/"+decodedToken._id, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
@@ -47,16 +50,19 @@ const UserHomeScreen = () => {
             },
         })
         const data = await resp.json();
-        console.log(data);
+        console.log("Data " + data[0][0]._id)
         setData(data);
         setLoading(false);
-    };
+      };
 
-    useEffect(() => {
-    fetchData();
-    const dataInterval = setInterval(() => fetchData(), 10 * 1000);
-    return () => clearInterval(dataInterval);
-    }, []);
+      /*
+        Used to update information
+      */
+      useEffect(() => {
+        fetchUserInfo();
+        const dataInterval = setInterval(() => fetchUserInfo(), 10 * 1000);
+        return () => clearInterval(dataInterval);
+      }, []);
 
     return (
         <View>
@@ -66,6 +72,7 @@ const UserHomeScreen = () => {
                     setSearchPhrase={setSearchPhrase}
                     clicked={clicked}
                     setClicked={setClicked}
+                    
                 />
             </View>
             <View>
